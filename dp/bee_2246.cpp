5553370960ec6a40b8b1faa4f2773da8b5362418
Enriconi <gabriel.enriconi@gmail.com>
2023-09-01 01:52:16 -0300

    perf: optimize beecrowd 2246

--- dp/bee_2246.cpp
+++ dp/bee_2246.cpp
@@ -6,23 +6,25 @@ using namespace std;
 #define MAXW 1010
 
 int v, nM;
-int memo[MAXN][MAXW], moedas[MAXN];
-
-int dp(int i, int w) {
-  if (i == 0) return 0;
-  if (memo[i][w] != -1) return memo[i][w];
-  if (w > v) return memo[i][w] = dp(i - 1, w);
-  return memo[i][w] = max(dp(i - 1, w), dp(i - 1, w - moedas[i]) + moedas[i]);
-}
+int moedas[MAXW];
+bool dp[MAXN];
 
 int main()
 {
-  memset(moedas, 0, sizeof(moedas));
   ios_base::sync_with_stdio(false); cin.tie(NULL);
+  memset(moedas, 0, sizeof(moedas));
   cin >> v >> nM;
   for (int i = 0; i < nM; i++) cin >> moedas[i];
-  memset(memo, -1, sizeof(memo));
-  cout << dp(nM, v) << '\n';
+  dp[0] = true;
 
+  for (int i = 0; i < nM; i++) {
+    for (int j = v - moedas[i]; j >= 0; j--) {
+      if (dp[j]) dp[j + moedas[i]] = true;
+    }
+  }
+
+  if (dp[v]) cout << 'S' << '\n';
+  else cout << 'N' << '\n';
+  
   return 0;
 }
